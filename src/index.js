a6d0ac79572adc99a53a19c40db1a28463f85dce
Ivan Stetsenko <i.stetsenko1@gmail.com>
2018-03-16 23:32:25 +0700

    add empty main.scss, import it in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+// imports from vendors
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import './styles/main.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
