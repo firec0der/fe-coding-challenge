c8c4122716b82fa44a16ebfc89c365729cd0d0b5
joey <karpuch13@yahoo.com>
2018-03-20 17:38:53 +0200

    fix: added container for Header content, styles: fix layout, added layout width variable,

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -6,10 +6,12 @@ export default class Header extends React.PureComponent {
   render() {
     return (
       <div className="Header">
-        <div className="Header__text">Protetiko FE coding challenge</div>
-        <div className="Header__nav">
-          <Link to="/" className="Header__link">Home</Link>
-          <Link to="/orders" className="Header__link">Orders</Link>
+        <div className="Header__container">
+          <div className="Header__text">Protetiko FE coding challenge</div>
+          <div className="Header__nav">
+            <Link to="/" className="Header__link">Home</Link>
+            <Link to="/orders" className="Header__link">Orders</Link>
+          </div>
         </div>
       </div>
     )}
